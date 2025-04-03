7623dcbb1baec50f587ff600b3f2e8f4e2a6f9ae
JessielynJenisha30 <220701901@rajalakshmi.edu.in>
2025-04-03 12:08:44 +0530

    Update fragment2.kt

--- EXP 4/fragment2.kt
+++ EXP 4/fragment2.kt
@@ -34,7 +34,7 @@ class fragment2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment2, container, false)
+        return inflater.inflate(R.layout.Fragment2, container, false)
     }
 
     companion object {
